616c0c4fb535ea5218b00b0bb075cdb12fad91bf
bdurb <brian@rpmgreenville.com>
2018-07-29 20:59:32 -0700

    still working with async data rendering

--- app/routes/people.js
+++ app/routes/people.js
@@ -6,7 +6,11 @@ import axios from 'npm:axios'
 export default Route.extend({
   model() {
     axios.get('https://swapi.co/api/people').then((res) => {
+      console.log(res.data.results)
       return res.data.results
+    })
+    .catch((err) => {
+      console.log(err);
     })
     }
 });
