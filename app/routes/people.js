061146d670c0bb30332e032140f26d825979fcdb
bdurb <brian@rpmgreenville.com>
2018-07-29 19:27:55 -0700

    added ember fetch

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 export default Route.extend({
+  model() {
+    return fetch('https://swapi.co/api/people')
+    .then((res) => {
+      return res.results
+      console.log(res.results)
+    })
+  }
 });
